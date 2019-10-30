24acf3cf636217753f996e1ef68c6b55cc65b1e7
Joe O <j0liverko@gmail.com>
2019-10-30 16:29:20 -0500

    Mvp

--- github-users/src/App.js
+++ github-users/src/App.js
@@ -31,26 +31,14 @@ class App extends React.Component {
       
   }
 
-  
-
-
-//   (5) [{…}, {…}, {…}, {…}, {…}]
-// 0: {login: "TinySquid", id: 13441400, node_id: "MDQ6VXNlcjEzNDQxNDAw", avatar_url: "https://avatars1.githubusercontent.com/u/13441400?v=4", gravatar_id: "", …}
-// 1: {login: "falmatad", id: 42183720, node_id: "MDQ6VXNlcjQyMTgzNzIw", avatar_url: "https://avatars2.githubusercontent.com/u/42183720?v=4", gravatar_id: "", …}
-// 2: {login: "YeetOrBeYate", id: 40674349, node_id: "MDQ6VXNlcjQwNjc0MzQ5", avatar_url: "https://avatars1.githubusercontent.com/u/40674349?v=4", gravatar_id: "", …}
-// 3: {login: "sjeremich23", id: 51142646, node_id: "MDQ6VXNlcjUxMTQyNjQ2", avatar_url: "https://avatars3.githubusercontent.com/u/51142646?v=4", gravatar_id: "", …}
-// 4: {login: "Greyflanel", id: 47545416, node_id: "MDQ6VXNlcjQ3NTQ1NDE2", avatar_url: "https://avatars2.githubusercontent.com/u/47545416?v=4", gravatar_id: "", …}
-
-  
-
   render() {
     console.log(this.state.user)
     return (
       <div className="App">
         <section>
-        <UserCard name={this.state.user.login} 
-                  avatar={this.state.user.avatar_url}
-
+        <UserCard 
+          name={this.state.user.login} 
+          avatar={this.state.user.avatar_url}
         />
         </section>
         <section>
